5ed5390440d318fd7b4edbf549ceadaeb1993429
d-engler <151563438+d-engler@users.noreply.github.com>
2024-05-19 10:37:08 -0700

    Update tb

--- src/tbfindfirstone.sv
+++ src/tbfindfirstone.sv
@@ -5,13 +5,24 @@ module top;
     logic [N-1:0] In;
     logic V;
     logic [$clog2(N)-1:0] I;
+    logic Error = 0;
+    
+    function findFirst(logic [N-1:0]findMe);
+	    for (int i=N; i>0; i--)
+		begin
+			if(findMe[i])
+				return i;
+		end
+		return 0;
+	endfunction
+
 
     nBitFFO #(N) dut(In, V, I);
 
-    initial
-    begin
-        $monitor($time,,, "%b : %b %b ", In, V, I);
-    end
+   // initial
+   // begin
+    //    $monitor($time,,, "%b : %b %b ", In, V, I);
+    //end
 
     initial
     begin
@@ -19,8 +30,15 @@ module top;
         begin
             In = i;
             #10;
+	    if (I !==findFirst(In))
+	    begin
+		    $display("In = %b, Index = %b, Expected = %b, Valid = %b",In,I,findFirst(In));
+		    Error = 1;
+	    end
         end
         #10;
+	if (Error ===0)
+		$display("Sim completed with no errors!");
         $finish;
     end
 endmodule
